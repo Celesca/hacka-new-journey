7176071872df6e5ecf77d28baed6f151715ff94e
celesca <folk.sawit@gmail.com>
2025-04-18 19:41:04 +0700

    add: login page

---
+++ app/login/page.tsx
@@ -0,0 +1,10 @@
+import LoginForm from "@/components/LoginForm";
+
+export default function LoginPage() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-r from-yellow-400 to-orange-400 p-8">
+      <h1 className="text-4xl font-bold text-white mb-6">Login</h1>
+      <LoginForm />
+    </div>
+  );
+}
