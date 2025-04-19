9925b909c49c110fa53c4194fe125ada4dd4e171
celesca <folk.sawit@gmail.com>
2025-04-19 14:00:31 +0700

    add: Chat to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,17 +12,40 @@ export default function NavBar() {
 
         {/* Menu Links */}
         <ul className="hidden md:flex items-center space-x-8">
-          <li>
-            <Link href="/news" className="text-gray-700 hover:text-orange-500 transition">
-              News
-            </Link>
-          </li>
-          <li>
-            <Link href="/match" className="text-gray-700 hover:text-orange-500 transition">
-              Match (Tinder)
-            </Link>
-          </li>
-        </ul>
+  <li>
+    <Link href="/news" className="text-gray-700 hover:text-orange-500 transition">
+      News
+    </Link>
+  </li>
+  <li>
+    <Link href="/match" className="text-gray-700 hover:text-orange-500 transition">
+      Match
+    </Link>
+  </li>
+  <li>
+    <Link
+      href="/chat"
+      className="flex items-center text-gray-700 hover:text-orange-500 transition"
+    >
+      {/* chat bubble icon */}
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M8 10h.01M12 10h.01M16 10h.01M21 12c0 4.418-4.03 8-9 8a9.77 9.77 0 01-4-.8L3 20l1.2-4A7.97 7.97 0 013 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"
+        />
+      </svg>
+      <span className="ml-1">Chat</span>
+    </Link>
+  </li>
+</ul>
 
         {/* Profile */}
         <div className="flex items-center space-x-4">
